a8ef1bf656cf971bbeda33c0cfbd8210b70d32e5
hoxi <hoxi_818@outlook.com>
2019-02-21 20:16:52 +0800

    框架搭建_注册部分短信验证码功能完善_发送间隔60s, 用redis管道执行命令

--- meiduo_mall/meiduo_mall/apps/verifications/views.py
+++ meiduo_mall/meiduo_mall/apps/verifications/views.py
@@ -9,7 +9,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from meiduo_mall.libs.yuntongxun.sms import CCP
-from verifications.constants import SMS_CODE_EXPIRES, SEND_SMS_TEMP_ID
+from verifications.constants import SMS_CODE_EXPIRES, SEND_SMS_TEMP_ID, SMS_CODE_SEND
 
 import logging
 logger = logging.getLogger('django')
@@ -23,24 +23,33 @@ class SMSCodeView(APIView):
         3.时运云通讯给mobile发送消息
         4.返回应答,短信发送结果
         """
+        redis_conn = get_redis_connection('verify_codes')
+        send_flag = redis_conn.get("sms_send_%s" % mobile)
+
+        if send_flag:
+            return Response({"message": "发送短信验证码过于频繁"})
+
         # 1.随机生成六位验证码
         sms_code = "%06d" % random.randint(0, 999999)
 
         # 2.在Redis中存储短信验证码对象 k:sms_<mobile> v:sms_code
-        redis_conn = get_redis_connection('verify_codes')
 
+        pl = redis_conn.pipeline()
         # <key> <expires> <value>
-        redis_conn.setex("sms_%s" % mobile, SMS_CODE_EXPIRES, sms_code)
+        pl.setex("sms_%s" % mobile, SMS_CODE_EXPIRES, sms_code)
+        pl.setex("sms_send_%s" % mobile, SMS_CODE_SEND, 1)
+
+        pl.execute()
 
         # 3.使用云通讯给mobile发送消息
-        expires = SMS_CODE_EXPIRES // 60
-        try:
-            res = CCP().send_template_sms("sms_%s" % mobile, [sms_code, expires], SEND_SMS_TEMP_ID)
-        except Exception as e:
-            logger.error(e)
-            return Response({'message':'验证码发送异常'}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
-        if res != 0:
-            return Response({'message':"验证码发送失败"}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
+        # expires = SMS_CODE_EXPIRES // 60
+        # try:
+        #     res = CCP().send_template_sms("sms_%s" % mobile, [sms_code, expires], SEND_SMS_TEMP_ID)
+        # except Exception as e:
+        #     logger.error(e)
+        #     return Response({'message':'验证码发送异常'}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
+        # if res != 0:
+        #     return Response({'message':"验证码发送失败"}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
 
         # 4.返回应答,短信发送结果
         return Response({'message': 'OK'})
