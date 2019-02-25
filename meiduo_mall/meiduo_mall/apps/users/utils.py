ddf18f5612d3e89d6046d1c546dfd39ecbd4e9ef
hoxi <hoxi_818@outlook.com>
2019-02-26 01:54:06 +0800

    注册部分_用户登录_自定义Django认证系统的后端类

--- meiduo_mall/meiduo_mall/apps/users/utils.py
+++ meiduo_mall/meiduo_mall/apps/users/utils.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.backends import ModelBackend
+from django.db.models import Q
+
+from users.models import User
+
+
 def jwt_response_payload_handler(token, user=None, request=None):
     """
     自定义响应方法
@@ -7,3 +13,18 @@ def jwt_response_payload_handler(token, user=None, request=None):
         'username': user.username,
         'token': token
     }
+
+# 自定义Django认证后端类
+class UsernameMobileAuthBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        """
+        username: 用户名或手机号
+        password: 密码
+        """
+        try:
+            user = User.objects.get(Q(username=username) | Q(mobile=username))
+        except User.DoesNotExist:
+            return None
+
+        if user.check_password(password):
+            return user
