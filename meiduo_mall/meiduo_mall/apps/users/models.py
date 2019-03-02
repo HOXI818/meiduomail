2597f053b3257bffa8091764bc20870ec8781d3c
hoxi <hoxi_818@outlook.com>
2019-03-02 14:54:08 +0800

    地址管理-地址模型类创建&默认地址字段添加

--- meiduo_mall/meiduo_mall/apps/users/models.py
+++ meiduo_mall/meiduo_mall/apps/users/models.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from itsdangerous import TimedJSONWebSignatureSerializer as TJWSSerializer
 from itsdangerous import BadData
 # Create your models here.
+from meiduo_mall.utils.models import BaseModel
 from users import constants
 
 
@@ -12,6 +13,8 @@ class User(AbstractUser):
     """用户模型类"""
     mobile = models.CharField(max_length=11, unique=True, verbose_name='手机号')
     email_active = models.BooleanField(default=False, verbose_name='邮箱验证状态')
+    default_address = models.ForeignKey('Address', related_name='users', null=True, blank=True,
+                                        on_delete=models.SET_NULL, verbose_name='默认地址')
 
     class Meta:
         db_table = 'tb_users'
@@ -58,4 +61,27 @@ class User(AbstractUser):
                 return None
             else:
                 return user
+
+
+class Address(BaseModel):
+    """
+    用户地址
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='addresses', verbose_name='用户')
+    title = models.CharField(max_length=20, verbose_name='地址名称')
+    receiver = models.CharField(max_length=20, verbose_name='收货人')
+    province = models.ForeignKey('areas.Area', related_name='province_address', on_delete=models.PROTECT, verbose_name='省')
+    city = models.ForeignKey('areas.Area', related_name='city_address', on_delete=models.PROTECT, verbose_name='市')
+    district = models.ForeignKey('areas.Area', related_name='district_address', on_delete=models.PROTECT, verbose_name='区')
+    place = models.CharField(max_length=50, verbose_name='地址')
+    mobile = models.CharField(max_length=11, verbose_name='手机')
+    tel = models.CharField(max_length=20, null=True, blank=True, default='', verbose_name='固定电话')
+    email = models.CharField(max_length=30, null=True, blank=True, default='', verbose_name='电子邮箱')
+    is_deleted = models.BooleanField(default=False, verbose_name='逻辑删除')
+
+    class Meta:
+        db_table = 'tb_address'
+        verbose_name = '用户地址'
+        verbose_name_plural = verbose_name
+        ordering = ['-update_time']
 
