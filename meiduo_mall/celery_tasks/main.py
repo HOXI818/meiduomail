83cea8ec52aecbb238dbfdd6db0f739663670e29
HOXI818 <HOXI_818@outlook.com>
2019-04-13 17:38:36 +0800

    商品部分-商品详情页静态代码实现

--- meiduo_mall/celery_tasks/main.py
+++ meiduo_mall/celery_tasks/main.py
@@ -5,8 +5,13 @@ import os
 if not os.environ.get('DJANGO_SETTINGS_MODULE'):
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "meiduo_mall.settings.dev")
 
-celery_app = Celery("celery_tasks")
+# 创建Celery类的对象
+celery_app = Celery('celery_tasks')
 
+
+# 加载配置
 celery_app.config_from_object('celery_tasks.config')
 
-celery_app.autodiscover_tasks(['celery_tasks.sms', 'celery_tasks.email'])
+
+# 让celery worker启动时自动发现有哪些任务函数
+celery_app.autodiscover_tasks(['celery_tasks.sms', 'celery_tasks.email', 'celery_tasks.html'])
